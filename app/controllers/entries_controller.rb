29ca093c4254cae061c98bfb02ffb08a89a85bf0
deepakdev094 <dipakdev094.gmail.com>
2023-07-14 16:53:18 +0530

    Updated view and controller for entry table

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,31 @@
 class EntriesController < ApplicationController
+  require 'google/apis/calendar_v3'
+  CALENDAR_ID = 'saifuddin.s@preciousinfosystem.com'
+  
+  def index 
+    google_calendar_service = GoogleCalendarService.new
+    @events = google_calendar_service.list_events
+  end
+  
+  def new
+    @entry = User.first.entries.new
+  end
+  def create
+    @entry = User.first.entries.new(entry_params)
+    
+    if @entry.save
+      calendar_service = GoogleCalendarService.new
+      calendar_service.create_event(@entry)
+      redirect_to root_path, notice: 'Event created successfully!'
+      byebug
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def entry_params
+    params.require(:entry).permit(:start_time, :end_time, :summary, :description)
+  end
 end
